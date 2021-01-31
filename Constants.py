67328e9c820e24e5244ed2e28ea03ca0fcd43723
Pavel Polyakov <djeredian@gmail.com>
2021-01-31 21:25:33 +0300

    fix spaces

--- Constants.py
+++ Constants.py
@@ -5,8 +5,8 @@
 C_ACTION_BUTTON = "@action:button"
 C_INFO_STATUS = "@info:status"
 C_TOOLTIP = "@properties:tooltip"
-C_TITLE ="@info:title"
-C_LABEL ="@label"
+C_TITLE = "@info:title"
+C_LABEL = "@label"
 C_WARNING_STATUS = "@warning:status"
 C_PROGRESS = "@info:progress"
 
@@ -17,7 +17,7 @@ MANUAL_INSTANCES = "mkswifi/manual_instances"
 
 # Don't translate the XML
 DNT_MESSAGE = "Don't translate the XML tags <message>!"
-DNT_FILENAME ="Don't translate the XML tags <filename>!"
+DNT_FILENAME = "Don't translate the XML tags <filename>!"
 DNT_BOTH = "Don't translate the XML tags <filename> or <message>!"
 
 # DNT combined messages
@@ -26,8 +26,8 @@ C_INFO_STATUS_DNT1 = C_INFO_STATUS+" "+DNT_MESSAGE
 C_TOOLTIP_DNT1 = C_TOOLTIP+" "+DNT_MESSAGE
 
 # Errors
-ERROR_MESSAGE1= "Error: command can not send"
-ERROR_MESSAGE2="Error: Another file is uploading, please try later."
+ERROR_MESSAGE1 = "Error: command can not send"
+ERROR_MESSAGE2 = "Error: Another file is uploading, please try later."
 EXCEPTION_MESSAGE = "An exception occurred in network connection: %s"
 
 # Exists
